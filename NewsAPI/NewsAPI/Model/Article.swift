accff92ab5effdafbe009d9c011d7b37a01c5f7a
Cuong Do Hung <>
2020-03-14 10:43:33 +0700

    Add package dependency: RxAlamofire, Kingfisher. Add empty swift files

---
+++ NewsAPI/NewsAPI/Model/Article.swift
@@ -0,0 +1,11 @@
+//
+//  Article.swift
+//  NewsAPI
+//
+//  Created by Cuong Do Hung on 3/14/20.
+//  Copyright © 2020 Cuong Do Hung. All rights reserved.
+//
+
+class Article{
+    
+}
---
+++ NewsAPI/NewsAPI/ViewModel/NewsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NewsViewModel.swift
+//  NewsAPI
+//
+//  Created by Cuong Do Hung on 3/14/20.
+//  Copyright © 2020 Cuong Do Hung. All rights reserved.
+//
+
+/* The base class for HeadlineViewModel & CustomNewsViewModel*/
+
+class NewsViewModel {
+    
+}
